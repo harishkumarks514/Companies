9235f7f604a8c6a215f4c98a2f7b03ec65fa2c8d
Harish Kumar K S <44613619+harishkumarks514@users.noreply.github.com>
2026-03-17 23:11:00 +0530

    Add files via upload

---
+++ Synechron/KthLargest_MinPriority.java
@@ -0,0 +1,28 @@
+package Synechron;
+
+import java.util.PriorityQueue;
+
+public class KthLargest_MinPriority {
+
+	public static int findKthLargest(int[] nums, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int num : nums){
+            pq.add(num);
+
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+
+        System.out.println(findKthLargest(nums, k));
+    }
+}
