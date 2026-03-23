afc188d2430b1ccc58e7ad5ec9dab1e9ab64c417
Harish Kumar K S <44613619+harishkumarks514@users.noreply.github.com>
2026-03-23 18:50:04 +0530

    Add files via upload

---
+++ BinarySubarraysWithSum.java
@@ -0,0 +1,33 @@
+package LeetCode;
+import java.util.*;
+
+public class BinarySubarraysWithSum {
+
+    public static int numSubarraysWithSum(int[] nums, int goal) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            if (map.containsKey(sum - goal)) {
+                count += map.get(sum - goal);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 1, 0, 1};
+        int goal = 2;
+
+        int result = numSubarraysWithSum(nums, goal);
+        System.out.println("Number of subarrays: " + result);
+    }
+}
