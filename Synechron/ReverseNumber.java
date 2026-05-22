453a23478558488671fea4794cfe37de1f8c94e4
Harish Kumar K S <44613619+harishkumarks514@users.noreply.github.com>
2026-05-23 00:36:07 +0530

    Add files via upload

---
+++ Synechron/ReverseNumber.java
@@ -0,0 +1,20 @@
+package Synechron;
+//Input 123
+// Output 321
+public class ReverseNumber {
+public static void main(String[] args) {
+	
+
+	int num = 123;
+	int reverse = 0;
+	
+	while(num != 0)
+	{
+	int digit =num % 10;
+	reverse = reverse * 10 + digit;
+	num = num / 10;
+	
+	}
+	System.out.println(reverse);
+}
+}
